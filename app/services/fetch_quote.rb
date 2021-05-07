e419acc1def4642a155a8f484ee37c544cb98ea5
Daniel Lee <dlee@sheetmusicplus.com>
2021-05-07 16:29:20 -0700

    load companies in initializer

--- app/services/fetch_quote.rb
+++ app/services/fetch_quote.rb
@@ -4,7 +4,7 @@ class FetchQuote
   end
 
   def call
-    raise InvalidTickerError, "'#{@ticker}' is an invalid ticker" unless companies.keys.include?(@ticker)
+    raise InvalidTickerError, "'#{@ticker}' is an invalid ticker" unless valid_ticker?
 
     open_price = Faker::Commerce.price
     change = Faker::Number.between(from: 0.0, to: 0.3).round(3) * [-1, -1, 0, 1, 1, 1, 1].sample
@@ -21,49 +21,20 @@ class FetchQuote
 
   private
 
+  def valid_ticker?
+    $COMPANIES.keys.include?(@ticker)
+  end
+
   def factor(change)
     change.zero? ? 1 : (1 + change)
   end
 
   def ticker_color
-    companies[@ticker]['ticker_color']
+    $COMPANIES[@ticker]['ticker_color']
   end
 
   def company_name
-    companies[@ticker]['company_name']
-  end
-
-  def companies
-    {
-      'AAPL' => {
-        'company_name' => 'Apple Inc',
-        'ticker_color' => '#666666'
-      },
-      'AMC' => {
-        'company_name' => 'AMC Entertainment',
-        'ticker_color' => '#E32226'
-      },
-      'AMZN' => {
-        'company_name' => 'Amazon.com, Inc.',
-        'ticker_color' => '#C26C03'
-      },
-      'FB' => {
-        'company_name' => 'Facebook, Inc. Common Stock',
-        'ticker_color' => '#4267B2'
-      },
-      'MSFT' => {
-        'company_name' => 'Microsoft Corporation',
-        'ticker_color' => '#737373'
-      },
-      'NVDA' => {
-        'company_name' => 'NVIDIA Corporation',
-        'ticker_color' => '#5B9000'
-      },
-      'TSLA' => {
-        'company_name' => 'Tesla Inc',
-        'ticker_color' => '#E31937'
-      }
-    }.freeze
+    $COMPANIES[@ticker]['company_name']
   end
 end
 
---
+++ config/initializers/load_companies.rb
@@ -0,0 +1,19 @@
+class ConfigurationFileNotFound < RuntimeError; end
+
+$COMPANIES = begin
+  file = File.read(Rails.root.join('config','companies.json'))
+  raw = JSON.parse(file)
+
+  companies = {}
+
+  raw.each do |c|
+    companies[c['ticker']] = {
+      'company_name' => c['company_name'],
+      'ticker_color' => c['ticker_color']
+    }
+  end
+
+  companies
+rescue Errno::ENOENT => e
+  raise ConfigurationFileNotFound, 'config/companies.json not found.'
+end
